0d1a573a4cb4502f7b68d29d5f76882aff09ab87
Sefat Anam <sefatanam@gmail.com>
2025-04-17 19:11:44 +0600

    feat: Enhance course info component with accordion and edit functionality

--- src/app/pages/admin/course-info/course-info.component.ts
+++ src/app/pages/admin/course-info/course-info.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-course-info',
   standalone: true,
-  imports: [CommonModule, CdkAccordionModule],
+  imports: [CommonModule, CdkAccordionModule, MatIconModule],
   templateUrl: './course-info.component.html',
   styleUrl: './course-info.component.css',
 })
--- src/app/pages/admin/courses/courses.component.ts
+++ src/app/pages/admin/courses/courses.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css',
 })
